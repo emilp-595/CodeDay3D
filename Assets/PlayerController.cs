68e1eca59874eaa11844f12c05c8690357072fa2
Joel <joelrenish@gmail.com>
2017-05-20 20:42:31 -0800

    initial file upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    Quaternion rotation;
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.Translate(0f, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        //add what is there for Vertical to the other ones to move it those ways
+
+        if (Input.GetKeyDown("A")|| Input.GetKeyDown("left"))
+        {
+            transform.Rotate(Vector3.left*Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown("D")|| Input.GetKeyDown("right"))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime, Space.World);
+        }
+
+        /*if (e.KeyCode == "A")
+        {
+            transform.Rotate(0, 2, 0);
+        }
+        if (e.KeyCode == "D")
+        {
+            transform.Rotate(0, -2, 0);
+        }*/
+
+    }
+
+}
+
---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 45.0f;
+    private const float Y_ANGLE_MAX = 45.0f;
+
+    public Transform lookAT;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 50.0f;
+    private float currentX = 10.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+    Quaternion rotation;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        //Add currentY before currentX for the movement to work
+        camTransform.position = lookAT.position + rotation * dir;
+        camTransform.LookAt(lookAT.position);
+        transform.rotation = rotation;
+    }
+}
